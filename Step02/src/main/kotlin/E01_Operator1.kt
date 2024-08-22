bee960f49c957da559397594fb2cd0b5df457822
nam2626 <nam2626@naver.com>
2024-08-22 11:57:52 +0900

    연산자

---
+++ Step02/src/main/kotlin/E01_Operator1.kt
@@ -0,0 +1,34 @@
+fun main() {
+    //사칙 연산자
+    //  +   -   *   /   %
+    var a = 10
+    var b = 3
+
+    println(a + b)
+    println(a - b)
+    println(a * b)
+    println(a / b)
+    println(a % b)
+
+    println("-----------------")
+
+    var c = 10.0
+    var d = 3.0
+
+    println(c + d)
+    println(c - d)
+    println(c * d)
+    println(c / d)
+    println(c % d)
+
+    println("-----------------")
+    //형변환
+    //  toByte(), toShort(), toInt(), toDouble()....
+    var e = 10
+    var f = 3
+
+    var result = e / f
+    println(result)
+
+
+}
