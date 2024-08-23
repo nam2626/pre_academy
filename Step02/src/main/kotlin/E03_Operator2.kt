2b710e82d7f144c2565ba407b892a7ffb3bae116
nam2626 <nam2626@naver.com>
2024-08-23 09:56:18 +0900

    복합대입 연산자

---
+++ Step02/src/main/kotlin/E03_Operator2.kt
@@ -0,0 +1,16 @@
+fun main() {
+    /*
+    *   복합대입 연산자 --> 특정변수에 데이터를 계속 연산하여 누적할 때
+    *       +=  -=  *=  /=  %=
+    * */
+    var num = 10
+
+    num += 5 // num = num + 5
+    println(num)
+    num -= 3
+    println(num)
+    num *= 2
+    println(num)
+    println(-num)
+
+}
