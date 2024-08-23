5881afd18197110d439eae96ee73c11b5d59c95a
nam2626 <nam2626@naver.com>
2024-08-23 11:09:16 +0900

    관계 연산자

---
+++ Step02/src/main/kotlin/E06_Operator4.kt
@@ -0,0 +1,21 @@
+fun main() {
+    //관계(비교) 연산자
+    // >    <   >=  <=  ==  !=
+    var a = 10
+    var b = 3
+
+    println(a > b)
+    println(a < b)
+    println(a >= b)
+    println(a <= b)
+    println(a == b)
+    println(a != b)
+
+    println("-------")
+    var c = 1.2
+    var d = 1.1 + 0.1
+    println(c)
+    println(d)
+    println(c == d)
+
+}
