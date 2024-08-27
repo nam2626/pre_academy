eda22c79a846109c7d7c6b4548f14425fdf12923
nam2626 <nam2626@naver.com>
2024-08-27 10:06:51 +0900

    For 문

---
+++ Step04/src/main/kotlin/E04_For.kt
@@ -0,0 +1,13 @@
+fun main() {
+    //1~10까지 출력
+    for (i in 1..10){
+        print("$i ")
+    }
+    println()
+    //1~10까지 2씩 증가해서 출력
+    for (i in 1..10 step 2){
+        print("$i ")
+    }
+    println()
+
+}
